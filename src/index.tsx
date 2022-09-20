465f695dd34028b837f0bf441a304b3c8661512b
Kazuki Awasawa <16559662+kawasawa@users.noreply.github.com>
2022-09-20 21:58:05 +0900

    Monaco Editor を導入

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
